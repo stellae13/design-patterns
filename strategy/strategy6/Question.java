85b973ebe0b63e554eef542001cf6d52c1dfb0b7
Stella Garcia <stellae13@gmail.com>
2021-09-28 20:24:50 -0400

    Added comments

--- strategy/strategy6/Question.java
+++ strategy/strategy6/Question.java
@@ -1,20 +1,38 @@
 package strategy6;
 
+/**
+ * Question class
+ * the data associated with a single question
+ */
 public class Question {
-   private String question;
-   private String[] answers;
-   private int correctAnswer; 
+   private String question; // the question
+   private String[] answers; // the answer options
+   private int correctAnswer; // the index of the correct answer
 
+   /**
+    * Question constructor
+    * sets all of the attributes for a Question
+    * @param question String the question being asked
+    * @param ans1 String the first option
+    * @param ans2 String the second option
+    * @param ans3 String the third option
+    * @param ans4 String the fourth option
+    * @param correctAnswer indexOf the correct answer (if it is ans1, this will equal 0)
+    */
    public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer) {
-       answers = new String[4];
        this.question = question;
-       answers[0] = ans1;
-       answers[1] = ans2;
-       answers[2] = ans3;
-       answers[3] = ans4;
+       this.answers = new String[4]; // 4 possible correct answers
+       this.answers[0] = ans1;
+       this.answers[1] = ans2;
+       this.answers[2] = ans3;
+       this.answers[3] = ans4;
        this.correctAnswer = correctAnswer;
    }
-
+   /**
+    * toString method
+    * @return String the question printed out in the style
+    * specified by the assignment 
+    */
    public String toString() {
        return question + "\n" + 
               "1. " + answers[0] + "\n" +
@@ -23,10 +41,21 @@ public class Question {
               "4. " + answers[3];
    }
 
+   /**
+    * isCorrect method
+    * sees if the user selected the right answer out of the array of answers
+    * @param userAnswer int- what number the user seleceted
+    * @return boolean - true if the user selected the right answer, false otherwise
+    */
    public boolean isCorrect(int userAnswer) {
-       return (userAnswer-1) == this.correctAnswer;
+       return (userAnswer-1) == this.correctAnswer; // userAnswer - 1 because the answers start at 1 in the toString and 0 in the array
    }
 
+   /**
+    * getCorrectAnswer
+    * returns the String stored at [correctAnswer] (so the correct answer)
+    * @return String the correct answer for this question
+    */
    public String getCorrectAnswer() {
        return answers[this.correctAnswer];
    }
--- strategy/strategy6/TriviaGame.java
+++ strategy/strategy6/TriviaGame.java
@@ -3,79 +3,96 @@ package strategy6;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
+/**
+ * TriviaGame class
+ * Singleton - you can only have one instance 
+ * of this class
+ */
 public class TriviaGame {
-  private static TriviaGame triviaGame;
-  private int score;
-  private Random rand;
-  private Scanner reader;
-  private ArrayList<Question> questions;
+  private static TriviaGame triviaGame; // static singleton object
+  private int score; // user score
+  private Random rand; // in order to randomize the question selection
+  private Scanner reader; // read user input
+  private ArrayList<Question> questions; // arrayList of questions read in using the DataLoader
 
+  /**
+   * Private TriviaGame constructor
+   * private to ensure the Singleton design pattern
+   */
   private TriviaGame() {
-    questions = DataLoader.getTriviaQuestions();
-    score = 0;
-    rand = new Random();
-    reader = new Scanner(System.in);
-    System.out.println("Welcome to the Trivia Game!");
+    questions = DataLoader.getTriviaQuestions(); // loads the questions in from the text file
+    score = 0; // initializes the score
+    rand = new Random(); // initializes the Random object
+    reader = new Scanner(System.in); // initializes the Scanner object
+    System.out.println("Welcome to the Trivia Game!"); // Prints out welcome message
   }
 
   public static TriviaGame getInstance() {
-      triviaGame = new TriviaGame();
-      return triviaGame;
+      triviaGame = new TriviaGame(); // creates the one instance of the class
+      return triviaGame; // returns this instance
   }
 
+  /**
+   * Play method (void)
+   * Runs the game as long as the user wants
+   */
   public void play() {
-    boolean keepPlaying;
-    char answer;
-    boolean correct;
+    boolean keepPlaying; // will be used to continue or end the game
+    char answer; // the answer to if the user wants to keep playing
+    boolean correct; // did the user get the correct answer
 
     do {
-        correct = playRound();
+        correct = playRound(); // playRound returns true if they got it right and false otherwise
         if(correct)
             score++;
 
         do {
         System.out.print("(P)lay or (Q)uit: ");
-        answer = reader.next().charAt(0);
+        answer = reader.next().charAt(0); // reads in the char
 
         if(answer != 'P' && answer != 'Q')
-          System.out.println("Invalid entry! Please enter 'P' to keep playing or 'Q' to quit");
+          System.out.println("Invalid entry! Please enter 'P' to keep playing or 'Q' to quit"); // if they didn't enter P or Q
         
         } while (answer != 'P' && answer != 'Q');
 
         if(answer == 'P')
-            keepPlaying = true;
+            keepPlaying = true; // the user wants to keep playing
         else 
-            keepPlaying = false;  
+            keepPlaying = false; // the user does not want to keep playing
 
-    } while (keepPlaying);
-    
+    } while (keepPlaying); // while the user wants to keep playing
+
     if(score == 1)
-        System.out.println("You won " +  score + " game!");
+        System.out.println("You won " +  score + " game!"); // 1 win is singular
     else
-        System.out.println("You won " +  score + " games!");
+        System.out.println("You won " +  score + " games!"); // plural otherwise
     System.out.println("Thanks for playing! Goodbye!"); 
   }
 
+  /**
+   * playRound method
+   * plays a single round of trivia, asking one question
+   * @return boolean - true if the user got the answer correct, false if they got it wrong
+   */
   public boolean playRound() {
-      int userAnswer;
-      int ques = rand.nextInt(questions.size());
-      System.out.println(questions.get(ques));
-      do{
+      int userAnswer; // the answer the user selects (number)
+      int ques = rand.nextInt(questions.size()); // randomizes the question selection
+      System.out.println(questions.get(ques)); // prints out the randomized question 
 
+      do{
       System.out.println("Enter answer: ");
       userAnswer = reader.nextInt();
       
-      if(userAnswer > 4 || userAnswer < 1)
+      if(userAnswer > 4 || userAnswer < 1) // makes sure that the answer selection is valid 
         System.out.println("Invalid Entry! Please select an answer by entering 1, 2, 3, or 4");
 
       } while(userAnswer > 4 || userAnswer < 1);
       
-        if(questions.get(ques).isCorrect(userAnswer)) {
+        if(questions.get(ques).isCorrect(userAnswer)) { // if the user got it right 
             System.out.println("YAY! you got it right!");
             return true;
-        } else {
-            System.out.println("You got it wrong!");
+        } else { // if the user got it wrong
+            System.out.println("You got it wrong!"); 
             System.out.println("The correct answer is " + questions.get(ques).getCorrectAnswer());
             return false;
         }
