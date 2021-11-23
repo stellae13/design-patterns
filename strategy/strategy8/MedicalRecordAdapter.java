11d3db87e97a1749b6e9ee5038e44c05270958f9
Stella Garcia <stellae13@gmail.com>
2021-11-23 09:32:31 -0500

    Completed code

--- strategy/strategy8/MedicalRecordAdapter.java
+++ strategy/strategy8/MedicalRecordAdapter.java
@@ -1,5 +1,7 @@
 package strategy8;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -49,7 +51,30 @@ public class MedicalRecordAdapter implements MedicalRecord {
 
         ArrayList<String> history = record.getHistory();
         ArrayList<Visit> toReturn = new ArrayList<Visit>();
+        String date;
+        Date dateFormatted = null;
+        String well;
+        boolean wellBool;
+        String description;
         for (String visit : history) {
+            date = visit.substring(visit.indexOf("Visit: ") + 11, visit.indexOf("\n")); // need to convert
+            well = visit.substring((visit.indexOf("Well visit: ") + 12),
+                    visit.indexOf("\n", visit.indexOf("Well visit: "))); //
+            description = visit.substring(visit.indexOf("Comments: ") + 10,
+                    visit.indexOf("\n", visit.indexOf("Comments: ")));
+
+            try {
+                dateFormatted = new SimpleDateFormat("dd, MM, yyyy").parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            if (well.equalsIgnoreCase("true"))
+                wellBool = true;
+            else
+                wellBool = false;
+
+            toReturn.add(new Visit(dateFormatted, wellBool, description));
 
         }
         return toReturn;
@@ -57,7 +82,28 @@ public class MedicalRecordAdapter implements MedicalRecord {
     }
 
     public String toString() {
-        return record.toString();
+        String toReturn = "***** " + getFirstName() + " " + getLastName() + " *****\n";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        String birthday = simpleDateFormat.format(getBirthday());
+        toReturn += "Birthday: " + birthday + "\n";
+        String gender = record.getGender();
+        toReturn += "Gender: ";
+
+        if (gender.equalsIgnoreCase("male"))
+            toReturn += "Male" + "\n";
+        else
+            toReturn += "Female" + "\n";
+
+        toReturn += "Medical Visit History: \n";
+
+        if (getVisitHistory().size() == 0) {
+            toReturn += "No visits yet";
+        } else {
+            for (Visit visit : getVisitHistory()) {
+                toReturn += visit.toString() + "\n";
+            }
+        }
+        return toReturn;
     }
 
 }
